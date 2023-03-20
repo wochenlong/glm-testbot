afa5c79b2c17292399125ae7e296605d30375b1b
windsing <117965575+wochenlong@users.noreply.github.com>
2023-03-21 00:45:28 +0800

    Update index.ts
    最新版

--- glm-testbot/src/index.ts
+++ glm-testbot/src/index.ts
@@ -6,39 +6,79 @@ export const usage = `
 
 chatglm对话插件，需要安装额外的文件。
 
-文件在这里下载：[答疑](https://forum.koishi.xyz/t/topic/1089)
+文件在这里下载：[论坛](https://forum.koishi.xyz/t/topic/1089)
 
-url格式：https://你的服务器地址/chatglm?
+服务器地址1格式：'https://你的服务器地址/chatglm?',提问词“glm”
+
+服务器地址1格式：'http://你的公网ip/chat',提问词“glms”
 
 
+
+公益api（由t4wefan提供，填到服务器地址1）：https://api.chat.t4wefan.pub/chatglm?
 
 `;
 export interface Config {
   myServerUrl: string;
+  publicUrl: string;
 }
 
 export const Config: Schema<Config> = Schema.object({
-  myServerUrl: Schema.string().description("服务器的地址"),
+  myServerUrl: Schema.string().description("服务器地址1").default(""),
+  publicUrl: Schema.string().description("服务器地址2：有公网ip的填这个"),
 });
 
 export async function apply(ctx: Context, config: Config) {
-  ctx.command("glm", "向chatglm提问").action(async ({ session }, ...args) => {
-    return await ctx.http.get(
-      [config.myServerUrl, "msg=", args[0], "&usrid=", session.userId].join(""),
-      { responseType: "text" }
-    );
-  });
-}
+  function mathRandomInt(a, b) {
+    if (a > b) {
+      // Swap a and b to ensure a is smaller.
+      var c = a;
+      a = b;
+      b = c;
+    }
+    return Math.floor(Math.random() * (b - a + 1) + a);
+  }
+  var memory_id = mathRandomInt(1, 1000000);
 
-/*
-export function apply(ctx: Context, config: Config) {
-  const { url } = config;
-  ctx.command("glm <msg>", "向chatglm提问").action(async ({ session }, msg) => {
-    const res = await ctx.http.post(url, {
-      msg,
+  ctx
+    .command("glm", "向chatglm提问")
+    .usage("进阶：输入'glm 重置记忆 '即可将记忆清零")
+    .action(async ({ session }, ...args) => {
+      if (args[0] == "重置记忆" || args[0] == "重置对话") {
+        await session.send("已重置全局记忆");
+        memory_id = Number(memory_id) + 2;
+      } else {
+        return await ctx.http.get(
+          [
+            config.myServerUrl,
+            ["msg=", String(args[0]) + "", "&usrid=|public"].join(""),
+            "|channel_id=" + String(session.channelId),
+            [
+              "|usr_id=" + String(session.userId),
+              "|secret=",
+              memory_id,
+              "|",
+            ].join(""),
+            "",
+          ].join(""),
+          { responseType: "text" }
+        );
+      }
+      return null;
     });
-    console.log(res);
-    return res;
-  });
+
+  config.myServerUrl + "msg=";
+  String("" + String("&usrid=" + String("" + String("" + String(memory_id)))));
+
+  const cmd1 = ctx
+    .command("glms <msg>", "也是向chatglm提问")
+    .action(async ({ session }, msg) => {
+      const res = await ctx.http.post(config.publicUrl, {
+        msg,
+      });
+      console.log(res);
+      return res;
+    });
 }
+/*
+
 */
